5a75d816048f728e68af814a85b3b0521212c289
AgilAchmadRofudin <agil73571@gmail.com>
2018-05-14 10:49:17 +0700

    index.php

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pendaftaran Siswa-Siswi Baru SMK N 1 Raman Utara</title>
+    <title>Pendaftaran Siswa-Siswi Baru SMK N 1 Raman Utara</title>																				
 </head>
-
 <body>
+<center>
     <header>
         <h3>Pendaftaran Siswa-Siswi Baru SMK N 1 Raman Utara</h3>
     </header>
-
+	</center>
+	<center>
     <h4>Menu</h4>
+	</center>
     <nav>
 	
 	<?php if(isset($_GET['status'])): ?>
@@ -25,8 +27,10 @@
 <?php endif; ?>
 	
         <ul>
-            <li><a href="form_daftar.php">Daftar Baru</a></li>
-            <li><a href="daftar_siswa.php">Pendaftar</a></li>
+		<center>
+            <button><a href="form_daftar.php">Daftar Baru</a></button>
+            <button><a href="daftar_siswa.php">Pendaftar</a></button>
+			</center>
         </ul>
     </nav>
     </body>
